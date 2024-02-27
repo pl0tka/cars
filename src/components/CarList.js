6b01c090bcaccd71c936784551660c517549d1d5
plotva <huseva.a@gmail.com>
2024-02-27 14:42:06 +0100

    Add highlighting of matching cars when typing a name

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -3,11 +3,16 @@ import { removeCar } from '../store';
 
 function CarList() {
   const dispatch = useDispatch();
-  const cars = useSelector(({ cars: { data, searchTerm } }) => {
+  const { cars, name } = useSelector(({ cars: { data, searchTerm }, form }) => {
     // searching (filtering) logic
-    return data.filter((car) =>
+    const filteredCars = data.filter((car) =>
       car.name.toLowerCase().includes(searchTerm.toLowerCase())
     );
+
+    return {
+      cars: filteredCars,
+      name: form.name,
+    };
   });
 
   const handleCarDelete = (car) => {
@@ -15,8 +20,12 @@ function CarList() {
   };
 
   const renderedCars = cars.map((car) => {
+    // DECIDE IF THE CAR SHOULD BE HIGHLIGHTED
+    // state.form.name is needed
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+
     return (
-      <div key={car.id} className="panel">
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
         <p>
           {car.name} - ${car.cost}
         </p>
